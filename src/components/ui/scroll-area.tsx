56d163dd5bb0ce0220466e2f3b2501844bb917cd
Samuel Guillemet <samu.guillemet0@gmail.com>
2025-11-16 01:12:16 +0100

    ⬆️ Update dependencies

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -12,7 +12,7 @@ const ScrollArea = React.forwardRef<
     className={cn('relative overflow-hidden', className)}
     {...props}
   >
-    <ScrollAreaPrimitive.Viewport className="w-full h-full rounded-[inherit]">
+    <ScrollAreaPrimitive.Viewport className="rounded-[inherit] w-full h-full">
       {children}
     </ScrollAreaPrimitive.Viewport>
     <ScrollBar />
@@ -29,7 +29,7 @@ const ScrollBar = React.forwardRef<
     ref={ref}
     orientation={orientation}
     className={cn(
-      'flex touch-none select-none transition-colors',
+      'flex transition-colors touch-none select-none',
       orientation === 'vertical' &&
         'h-full w-2.5 border-l border-l-transparent p-px',
       orientation === 'horizontal' &&
@@ -38,7 +38,7 @@ const ScrollBar = React.forwardRef<
     )}
     {...props}
   >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border" />
+    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 bg-border rounded-full" />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
 ));
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName;
